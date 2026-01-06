4f9ab5295b2866040a8e8743527bb737ccb824ac
cpl010 <caiopuyolleguiza@gmail.com>
2026-01-06 15:00:57 -0300

    ci(GeoGuard): Puede validar la geolocalizacion guardada

---
+++ src/components/GeoGuard/index.tsx
@@ -0,0 +1,30 @@
+import React, { JSX, useEffect } from "react";
+import Swal from "sweetalert2";
+
+import { useSearchParams } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useGeo } from "../../hooks/geo-hooks";
+
+export const GeoGuard = ({ children }: { children: JSX.Element }) => {
+  const [sp] = useSearchParams();
+  const [geo,] = useGeo();
+
+  const hasLocation = sp.get("lat") && sp.get("lng");
+  useEffect(() => {
+    if (!hasLocation && !geo) {
+      Swal.fire({
+        icon: 'error',
+        title: `¡Se requiere ubicación!`,
+        text: `Para continuar se requiere acceso a su ubicación`,
+        timer: 2000,
+        showConfirmButton: false
+      });
+    }
+  }, [hasLocation]);
+
+  if (!hasLocation && !geo) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
