9e61b5b74b5b114e93ffdb92aa86ca5548c3c39c
cpl010 <caiopuyolleguiza@gmail.com>
2026-01-06 15:02:34 -0300

    feat(/MisReportes): pagina completada.

---
+++ src/pages/MisReportes/index.tsx
@@ -0,0 +1,39 @@
+import React, { Suspense } from "react";
+
+import { useMyReports } from "../../hooks/user-hooks";
+import { useNavigate } from "react-router-dom";
+
+import * as css from './misreportes.module.css'
+
+import { PetWanted } from "../../types/pet";
+
+import { SignInGuard } from "../../components/SignInGuard";
+import { RowReportsV2 } from "../../components/rowReports";
+import { Spinner } from "../../components/spiner";
+import { PlussButton } from "../../components/PlussButton";
+
+export function MisReportesPage() {
+  const navigate = useNavigate();
+  let [nearReports,] = useMyReports();
+  if (!nearReports) nearReports = [];
+
+  const handleCardClick = (card:PetWanted) => {
+    navigate(`/editarreporte?card=${card.id}`)
+  };
+  const handlePlussButtonClick = () => {
+    navigate("/reportarnuevamascota");
+  };
+  
+  return (
+    <SignInGuard>
+      <>
+        <div className={css.misreportes}>
+          <Suspense fallback={<Spinner /> }>
+            <RowReportsV2 cards={nearReports} onClick={handleCardClick} />
+          </Suspense>
+        </div>
+        <PlussButton onClick={handlePlussButtonClick}/>
+      </>
+    </SignInGuard>
+  );
+}
