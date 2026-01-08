335ced63ee7c5ea1e799c9ab0f8f3936eb0f2004
cpl010 <caiopuyolleguiza@gmail.com>
2026-01-08 17:14:00 -0300

    fix: rename to temp

--- src/components/PlussButtonTemp/index.tsx
+++ src/components/PlussButtonTemp/index.tsx
@@ -3,7 +3,8 @@ import * as css from "./pluss.module.css";
 
 const plusButtonClases = `btn btn-primary btn-lg rounded-circle shadow-lg position-fixed ${css.plusbutton}`;
 
-export function PlussButton({ onClick }: {onClick: () => void}) {
+export function PlussButton({ onClick }: { onClick: () => void }) {
+  
   return (
     <button className={plusButtonClases} onClick={onClick}>
       <i className="bi bi-plus-lg"></i>
